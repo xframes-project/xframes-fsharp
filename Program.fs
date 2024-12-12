517d3b946741b90e8f927385646fe9f51cdcc2c8
Andrea Mancuso <and.mancuso@googlemail.com>
2024-12-12 21:38:02 +0100

    Update .gitignore, ProjectSettings.json, and 38 more files...

--- Program.fs
+++ Program.fs
@@ -1,2 +1,41 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿// Program.fs
+open System
+
+type Widget = {
+    Id: int
+    Type: string
+    mutable Props: Map<string, obj>
+    mutable Children: Widget list
+}
+
+let createNode id type' props children =
+    {
+        Id = id
+        Type = type'
+        Props = props
+        Children = children
+    }
+
+
+[<EntryPoint>]
+let main argv =
+    
+    // Example of creating a Node widget containing a Button and a Label widget
+    let buttonWidget = {
+        Id = 1
+        Type = "Button"
+        Props = Map.ofList [("text", "Click Me")]
+        Children = []
+    }
+
+    let labelWidget = {
+        Id = 2
+        Type = "Label"
+        Props = Map.ofList [("text", "Hello World")]
+        Children = []
+    }
+
+    let nodeWidget = createNode 3 "Node" (Map.ofList [("style", "vertical")]) [buttonWidget; labelWidget]
+
+    printfn "Hello, F#"
+    0 // return an integer exit code
--- obj/Debug/net9.0/xframes.AssemblyInfo.fs
+++ obj/Debug/net9.0/xframes.AssemblyInfo.fs
@@ -10,7 +10,7 @@ open System.Reflection
 [<assembly: System.Reflection.AssemblyCompanyAttribute("xframes")>]
 [<assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")>]
 [<assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")>]
-[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5111aeedef3b7152aa21446b0529721a37e49620")>]
+[<assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+724dc3df0c86261d48c7a77317e7512454842748")>]
 [<assembly: System.Reflection.AssemblyProductAttribute("xframes")>]
 [<assembly: System.Reflection.AssemblyTitleAttribute("xframes")>]
 [<assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")>]
