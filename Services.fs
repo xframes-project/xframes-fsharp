4dd905062255d999a5a27d997214e9ea3d536ba8
Andrea Mancuso <and.mancuso@googlemail.com>
2024-12-21 20:43:59 +0100

    Update .gitignore, Services.fs, and xframes.fsproj.nuget.dgspec.json

--- Services.fs
+++ Services.fs
@@ -86,12 +86,12 @@ module WidgetRegistrationService =
         // Placeholder for style retrieval
         ""
 
-    let setData (id: int) (data: List<obj>) =
+    let setData (id: int, data: List<obj>) =
         //let opDictionary = SetData
         //opDictionary.Add("")
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let appendData (id: int) (data: List<obj>) =
+    let appendData (id: int, data: List<obj>) =
         //let jsonData = JsonAppendData(Op = "appendData", Data = data)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
@@ -99,25 +99,25 @@ module WidgetRegistrationService =
         //let jsonData = JsonResetData(Op = "resetData")
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let appendDataToPlotLine (id: int) (x: float) (y: float) =
+    let appendDataToPlotLine (id: int, x: float, y: float) =
         //let jsonData = JsonAppendDataToPlotLine(Op = "appendData", X = x, Y = y)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let setPlotLineAxesDecimalDigits (id: int) (x: float) (y: float) =
+    let setPlotLineAxesDecimalDigits (id: int, x: float, y: float) =
         //let jsonData = JsonSetAxesDecimalDigits(Op = "setAxesDecimalDigits", X = x, Y = y)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let setAxisAutoFitEnabled (id: int) (enabled: bool) =
+    let setAxisAutoFitEnabled (id: int, enabled: bool) =
         //let jsonData = JsonSetAxesAutoFit(Op = "setAxesAutoFit", Enabled = enabled)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let appendTextToClippedMultiLineTextRenderer (id: int) (text: string) =
-        appendTextToClippedMultiLineTextRenderer(id, text)
+    let appendTextToClippedMultiLineTextRenderer (id: int, text: string) =
+        Externs.appendTextToClippedMultiLineTextRenderer(id, text)
 
-    let setInputTextValue (id: int) (value: string) =
+    let setInputTextValue (id: int, value: string) =
         //let jsonData = JsonSetValue(Op = "setValue", Value = value)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
 
-    let setComboSelectedIndex (id: int) (index: int) =
+    let setComboSelectedIndex (id: int, index: int) =
         //let jsonData = JsonSetSelectedIndex(Op = "setSelectedIndex", Index = index)
         elementInternalOp(id, JsonConvert.SerializeObject(""))
